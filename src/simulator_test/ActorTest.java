37e607a21b9d751d1a6e897d321c0807cf0cf495
robertpivie <ivier@studentbody.byu.edu>
2014-02-09 20:46:34 -0700

    Started unit test class.

---
+++ src/simulator_test/ActorTest.java
@@ -0,0 +1,84 @@
+package simulator_test;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class ActorTest {
+
+	@Test
+	public final void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testInitializeInternalVariables() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetEnabledTransitions() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testUpdateTransitions() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetCurrentState() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetInternalVariable() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetInternalVars() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetSubActors() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAddSubActor() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testStartState() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAdd() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testEqualsObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
