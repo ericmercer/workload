4fb6c67a269e1f577e6f49ba2ab3fa638463e10f
lilkicker <filoso.michael@gmail.com>
2014-05-21 13:10:06 -0600

    JPF does not support Runtime.exec, made it so if you are running JPF it doesnt try to run the post proccessor for that reason.  Fixed finding the current directory as well.

--- src/model/WiSARModel.java
+++ src/model/WiSARModel.java
@@ -18,7 +18,7 @@ public class WiSARModel {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws InterruptedException, IOException {
-		RunsJPF forJPF = RunsJPF.NO;
+		RunsJPF forJPF = RunsJPF.YES;
 		int times = (forJPF == RunsJPF.NO) ? 500: 1;
 		for(int i = 0; i < times; i++)
 		{
@@ -32,17 +32,21 @@ public class WiSARModel {
 			String checkSafe = sim.run();
 			sim.clean();
 		}
-				
-		File f = null;
-		try {
-			f = new File(new File(WiSARModel.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParent());
-		} catch (URISyntaxException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		
+		
+		if(forJPF == RunsJPF.NO)
+		{
+			String f = null;
+			try {
+				f = new File(".").getAbsolutePath();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		
+			String python = f;
+			python+=File.separator+"PostProcessing.py";
+			Process p = Runtime.getRuntime().exec("python "+python +" 9");
 		}
-
-		String python = f.toString();
-		python+=File.separator+"PostProcessing.py";
-		Process p = Runtime.getRuntime().exec("python "+python +" 9");
 	}	
 }
